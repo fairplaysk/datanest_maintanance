070bc27d1f2586906df7026c61a8ed1011539e5f
Michal Olah <olahmichal@gmail.com>
2010-09-26 22:22:46 +0200

    refactoring most of the app class app to lib

---
+++ lib/app.rb
@@ -0,0 +1,97 @@
+class App
+  VERSION = '0.0.1'
+  
+  attr_reader :options
+
+  def initialize(arguments, stdin)
+    @arguments = arguments
+    @stdin = stdin
+    
+    # Set defaults
+    @options = OpenStruct.new
+    @options.verbose = false
+    @options.quiet = false
+  end
+
+  # Parse options, check arguments, then process the command
+  def run
+        
+    if parsed_options? && arguments_valid? 
+      
+      puts "Start at #{DateTime.now}\n\n" if @options.verbose
+      
+      output_options if @options.verbose # [Optional]
+            
+      process_arguments            
+      process_command
+      
+      puts "\nFinished at #{DateTime.now}" if @options.verbose
+      
+    else
+      output_usage
+    end
+      
+  end
+  
+  protected
+  
+    def parsed_options?
+      
+      # Specify options
+      opts = OptionParser.new 
+      opts.on('-v', '--version')    { output_version ; exit 0 }
+      opts.on('-h', '--help')       { output_help }
+      opts.on('-V', '--verbose')    { @options.verbose = true }  
+      opts.on('-q', '--quiet')      { @options.quiet = true }
+      # TO DO - add additional options
+            
+      opts.parse!(@arguments) rescue return false
+      
+      process_options
+      true      
+    end
+
+    # Performs post-parse processing on options
+    def process_options
+      @options.verbose = false if @options.quiet
+    end
+    
+    def output_options
+      puts "Options:\n"
+      
+      @options.marshal_dump.each do |name, val|        
+        puts "  #{name} = #{val}"
+      end
+    end
+
+    # True if required arguments were provided
+    def arguments_valid?
+      true
+    end
+    
+    # Setup the arguments
+    def process_arguments
+    end
+    
+    def output_help
+      output_version
+      RDoc::usage_no_exit #exits app
+    end
+    
+    def output_usage
+      #RDoc::usage('usage') # gets usage from comments above
+    end
+    
+    def output_version
+      puts "#{File.basename(__FILE__)} version #{VERSION}"
+    end
+    
+    def process_standard_input
+      input = @stdin.read      
+      # TO DO - process input
+      
+      @stdin.each do |line| 
+        puts line
+      end
+    end
+end
--- move_column.rb
+++ move_column.rb
@@ -39,143 +39,48 @@ require 'ostruct'
 require 'date'
 require 'active_support/inflector'
 require './lib/datanest'
+require './lib/app'
 
 class App
-  VERSION = '0.0.1'
+  def process_command
   
-  attr_reader :options
-
-  def initialize(arguments, stdin)
-    @arguments = arguments
-    @stdin = stdin
-    
-    # Set defaults
-    @options = OpenStruct.new
-    @options.verbose = false
-    @options.quiet = false
-  end
-
-  # Parse options, check arguments, then process the command
-  def run
-        
-    if parsed_options? && arguments_valid? 
-      
-      puts "Start at #{DateTime.now}\n\n" if @options.verbose
-      
-      output_options if @options.verbose # [Optional]
-            
-      process_arguments            
-      process_command
-      
-      puts "\nFinished at #{DateTime.now}" if @options.verbose
-      
-    else
-      output_usage
+    while true do
+      table = init
+      break if table
     end
-      
-  end
   
-  protected
+    create_activerecord_model(table.singularize.capitalize)
+    model = table.capitalize.classify.constantize
   
-    def parsed_options?
-      
-      # Specify options
-      opts = OptionParser.new 
-      opts.on('-v', '--version')    { output_version ; exit 0 }
-      opts.on('-h', '--help')       { output_help }
-      opts.on('-V', '--verbose')    { @options.verbose = true }  
-      opts.on('-q', '--quiet')      { @options.quiet = true }
-      # TO DO - add additional options
-            
-      opts.parse!(@arguments) rescue return false
-      
-      process_options
-      true      
-    end
-
-    # Performs post-parse processing on options
-    def process_options
-      @options.verbose = false if @options.quiet
-    end
-    
-    def output_options
-      puts "Options:\n"
-      
-      @options.marshal_dump.each do |name, val|        
-        puts "  #{name} = #{val}"
-      end
-    end
-
-    # True if required arguments were provided
-    def arguments_valid?
-      true
-    end
-    
-    # Setup the arguments
-    def process_arguments
-    end
-    
-    def output_help
-      output_version
-      RDoc::usage_no_exit #exits app
-    end
-    
-    def output_usage
-      #RDoc::usage('usage') # gets usage from comments above
-    end
-    
-    def output_version
-      puts "#{File.basename(__FILE__)} version #{VERSION}"
-    end
-    
-    def process_command
-      
-      while true do
-        table = init
-        break if table
-      end
-      
-      create_activerecord_model(table.singularize.capitalize)
-      model = table.capitalize.classify.constantize
-      
-      master_column = get_master_column
-      test_column(model, master_column) do
-        target_column = get_target_column
-        test_column(model, target_column) do
-          model.all.each_with_index do |row, index|
-            if row != nil
-              update_decision(model, master_column, target_column)
-            elsif
-              model.send("#{target_column}=", model.send(master_column))
-              model.save!
-            end
-            puts "Spracovanych #{index+1} riadkov." if index % 20 == 0
+    master_column = get_master_column
+    test_column(model, master_column) do
+      target_column = get_target_column
+      test_column(model, target_column) do
+        model.all.each_with_index do |row, index|
+          if row != nil
+            update_decision(model, master_column, target_column)
+          elsif
+            model.send("#{target_column}=", model.send(master_column))
+            model.save!
           end
-          puts "Spracovanie dat ukoncene. Celkovo bolo spracovanych #{model.count} riadkov."
+          puts "Spracovanych #{index+1} riadkov." if index % 20 == 0
         end
+        puts "Spracovanie dat ukoncene. Celkovo bolo spracovanych #{model.count} riadkov."
       end
-      
-      #process_standard_input
     end
-    
-    def update_decision(model, master, target)
-      choose do |menu|
-        menu.prompt = "Hodnota v stlpci `target` je nenulova. Co si zelate spravit?"
+  
+    #process_standard_input
+  end
 
-        menu.choice('prepisat') { model.send("#{target_column}=", model.send(master_column)); model.save!; }
-        menu.choices('preskocit')
-        menu.choices('ukoncit program'){ exit }
-      end
+  def update_decision(model, master, target)
+    choose do |menu|
+      menu.prompt = "Hodnota v stlpci `target` je nenulova. Co si zelate spravit?"
+
+      menu.choice('prepisat') { model.send("#{target_column}=", model.send(master_column)); model.save!; }
+      menu.choices('preskocit')
+      menu.choices('ukoncit program'){ exit }
     end
-    
-    def process_standard_input
-      input = @stdin.read      
-      # TO DO - process input
-      
-      @stdin.each do |line| 
-        puts line
-      end
-    end
+  end
 end
 
 
